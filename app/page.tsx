33132e1695509d9876324a645279f45c3ce5b9c1
AnasOsta <ensusta1@gmail.com>
2025-05-13 01:08:08 +0300

    Add new avatar and icon assets
    - Added new PNG avatar: image-ramsesmiron.png
    - Added new WEBP avatar: image-ramsesmiron.webp
    - Updated favicon with new PNG format: favicon.png
    - Introduced new SVG icons: icon-delete.svg, icon-edit.svg, icon-minus.svg, icon-plus.svg, icon-reply.svg

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,120 @@
+import data from "@/data.json";
 import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="flex flex-col gap-4 py-16 relative">
+      {data.comments.map((comment) => (
+        <div key={comment.id} className="max-w-2xl mx-auto ">
+          <div className="px-4 flex gap-x-4 bg-white p-4 rounded-xl">
+            <div className="flex flex-col items-center gap-y-4 bg-[#F5F7FA] text-[#5357B6] font-bold h-fit rounded-lg p-2">
+              <Image
+                src="/icon-plus.svg"
+                alt="amyrobson"
+                width={40}
+                height={40}
+              />
+              <p>{comment.score}</p>
+              <Image
+                src="/icon-minus.svg"
+                alt="amyrobson"
+                width={40}
+                height={40}
+              />
+            </div>
+            <div className="flex flex-col gap-y-4">
+              <div className="flex justify-between items-center">
+                <div className="flex gap-x-4">
+                  <Image
+                    src={comment.user.image.png}
+                    alt={comment.user.username}
+                    width={40}
+                    height={40}
+                  />
+                  <p className="font-bold">{comment.user.username}</p>
+                  <p className="text-[#828FA3]">{comment.createdAt}</p>
+                </div>
+                <div className="flex items-center gap-2 text-[#5357B6] font-bold">
+                  <Image
+                    src="/icon-reply.svg"
+                    alt="amyrobson"
+                    width={15}
+                    height={15}
+                  />
+                  <span>Reply</span>
+                </div>
+              </div>
+              <p className="text-[#828FA3]">{comment.content}</p>
+            </div>
+          </div>
+          {comment.replies.length > 0 && (
+            <div className="flex flex-col gap-y-4 mt-4 border-l-2 border-gray-300 mx-6">
+              {comment.replies.map((reply) => (
+                <div
+                  key={reply.id}
+                  className="px-4 flex gap-x-4 bg-white p-4 max-w-xl mx-auto rounded-xl"
+                >
+                  <div className="flex flex-col items-center gap-y-4 bg-[#F5F7FA] text-[#5357B6] font-bold h-fit rounded-lg p-2">
+                    <Image
+                      src="/icon-plus.svg"
+                      alt="amyrobson"
+                      width={40}
+                      height={40}
+                    />
+                    <p>{reply.score}</p>
+                    <Image
+                      src="/icon-minus.svg"
+                      alt="amyrobson"
+                      width={40}
+                      height={40}
+                    />
+                  </div>
+                  <div className="flex flex-col gap-y-4">
+                    <div className="flex justify-between items-center">
+                      <div className="flex gap-x-4">
+                        <Image
+                          src={reply.user.image.png}
+                          alt={reply.user.username}
+                          width={40}
+                          height={40}
+                        />
+                        <p className="font-bold">{reply.user.username}</p>
+                        <p className="text-[#828FA3]">{reply.createdAt}</p>
+                      </div>
+                      <div className="flex items-center gap-2 text-[#5357B6] font-bold">
+                        <Image
+                          src="/icon-reply.svg"
+                          alt="amyrobson"
+                          width={15}
+                          height={15}
+                        />
+                        <span>Reply</span>
+                      </div>
+                    </div>
+                    <p className="text-[#828FA3]">{reply.content}</p>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
+      <div className="fixed bottom-0 right-0 left-0 bg-white rounded-lg flex gap-x-4 p-4 max-w-2xl mx-auto">
+        <Image
+          src={data.currentUser.image.png}
+          alt="user image"
+          width={40}
+          height={40}
+          className="rounded-full h-fit"
+        />
+        <textarea
+          placeholder="Add a comment..."
+          className="w-full h-32 border border-gray-300 rounded-lg p-4 focus:border-[#5357B6] focus:outline-none resize-none"
+        />
+
+        <button className="bg-[#5357B6] text-white px-6 py-3 rounded-lg h-fit cursor-pointer hover:bg-[#5357B6]/80 active:opacity-90 select-none">
+          SEND
+        </button>
+      </div>
     </div>
   );
 }
